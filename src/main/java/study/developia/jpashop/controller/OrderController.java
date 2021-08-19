617a8f419ebf722afbfc9862fba3bedef1dd6500
developiaa <developiaa@gmail.com>
2021-08-19 20:15:51 +0900

    feat : Add order controller

---
+++ src/main/java/study/developia/jpashop/controller/OrderController.java
@@ -0,0 +1,42 @@
+package study.developia.jpashop.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import study.developia.jpashop.domain.Member;
+import study.developia.jpashop.domain.item.Item;
+import study.developia.jpashop.service.ItemService;
+import study.developia.jpashop.service.MemberService;
+import study.developia.jpashop.service.OrderService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/oeswe")
+    public String order(@RequestParam("memberId") Long memberId, @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
