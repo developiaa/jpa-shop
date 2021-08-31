f251cff99960822cedc28ea7a8f5a0381b794d06
developiaa <developiaa@gmail.com>
2021-08-31 22:17:38 +0900

    feat : Add api controller

---
+++ src/main/java/study/developia/jpashop/api/MemberApiController.java
@@ -0,0 +1,32 @@
+package study.developia.jpashop.api;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import study.developia.jpashop.domain.Member;
+import study.developia.jpashop.service.MemberService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse{
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
