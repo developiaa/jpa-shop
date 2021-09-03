9cb328e4c1f4be5f140ad5815a3b8922f7e08eb6
developiaa <developiaa@gmail.com>
2021-09-03 22:48:21 +0900

    feat : Add api with dto

--- src/main/java/study/developia/jpashop/api/MemberApiController.java
+++ src/main/java/study/developia/jpashop/api/MemberApiController.java
@@ -16,13 +16,26 @@ public class MemberApiController {
     private final MemberService memberService;
 
     @PostMapping("/api/v1/members")
-    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
 
     @Data
-    static class CreateMemberResponse{
+    static class CreateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
         private Long id;
 
         public CreateMemberResponse(Long id) {
