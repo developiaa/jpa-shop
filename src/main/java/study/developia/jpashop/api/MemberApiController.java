2c37a4ea0689d55a3f8707f1dfc1d9eec3d2f77e
developiaa <developiaa@gmail.com>
2021-09-03 23:01:50 +0900

    feat : Add member update api

--- src/main/java/study/developia/jpashop/api/MemberApiController.java
+++ src/main/java/study/developia/jpashop/api/MemberApiController.java
@@ -1,10 +1,9 @@
 package study.developia.jpashop.api;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import study.developia.jpashop.domain.Member;
 import study.developia.jpashop.service.MemberService;
 
@@ -28,6 +27,26 @@ public class MemberApiController {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdatememberResponse updateMemberV2(@PathVariable("id") Long id, @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdatememberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdatememberResponse {
+        private Long id;
+        private String name;
+    }
+
 
     @Data
     static class CreateMemberRequest {
